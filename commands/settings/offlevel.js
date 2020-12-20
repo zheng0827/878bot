cac9da2409a6c4cffb06dfe641d5e258a78a650c
zheng0827 <76244161+zheng0827@users.noreply.github.com>
2020-12-20 20:07:33 +0800

    Create offlevel.js

---
+++ commands/settings/offlevel.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    name:"offlevel",
+
+    code:`成功關閉等級功能
+
+你可以使用\`$getServerVar[prefix]openlevel\`開啟等級功能
+
+Level function successfully closed
+
+You can use \`$getServerVar[prefix]openlevel\` to open the level function
+
+$setServerVar[leveloo;off]
+
+$onlyAdmin[$randomText[\`你不是管理員/You are not an administrator\`;\`請叫管理員設置/Please call the administrator to set\`;\`你沒有權限使用/You don't have permission to use\`]]`}
+
